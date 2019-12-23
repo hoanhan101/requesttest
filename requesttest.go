ce739beda2ba48a1b7f2af782f7e7150e0f7fbe5
Hoanh An <hoanhan@bennington.edu>
2019-12-23 16:38:12 -0500

    initial request test implmentation

---
+++ requesttest.go
@@ -0,0 +1,25 @@
+package requesttest
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Serve(path string, response interface{}) (string, func()) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+
+	mux.HandleFunc(
+		path,
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+			fmt.Fprint(w, response)
+		},
+	)
+
+	return fmt.Sprintf("%s%s", server.URL, path), server.Close
+}
