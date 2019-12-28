c0ed3aa8f9e73623e5885e9e1a0ce934551dae43
Hoanh An <hoanhan@bennington.edu>
2019-12-28 03:06:15 -0500

    add testable example

---
+++ example_test.go
@@ -0,0 +1,34 @@
+package requesttest
+
+import (
+	"fmt"
+
+	"github.com/hoanhan101/request"
+)
+
+// Response describes a response from requesttest.Echo().
+type Response struct {
+	Method  string `json:"method"`
+	Payload string `json:"payload"`
+}
+
+func ExampleEcho() {
+	url, closer := Echo()
+	defer closer()
+
+	r := new(Response)
+
+	err := request.GetJSON(
+		&request.Options{
+			URL:     url,
+			Payload: map[string]string{"k1": "v1"},
+		},
+		r,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%+v\n", r)
+	// Output: &{Method:GET Payload:k1=v1}
+}
