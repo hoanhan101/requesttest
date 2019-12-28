da61e483bd225728f2fcee75ee8bd19cbb501097
Hoanh An <hoanhan@bennington.edu>
2019-12-28 02:13:09 -0500

    echo request info

--- requesttest.go
+++ requesttest.go
@@ -6,12 +6,8 @@ import (
 	"net/http/httptest"
 )
 
-// Serve registers a route, overwrites its response and returns a ready-to-use URL.
-//
-// For example:
-//  url, closer := requesttest.Serve("/get", `{"status":"ok"}`)
-//  defer closer()
-func Serve(path string, response interface{}) (string, func()) {
+// Write a JSON response.
+func Write(path string, response interface{}) (string, func()) {
 	mux := http.NewServeMux()
 	server := httptest.NewServer(mux)
 
@@ -21,6 +17,34 @@ func Serve(path string, response interface{}) (string, func()) {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(200)
 			fmt.Fprint(w, response)
+		},
+	)
+
+	return fmt.Sprintf("%s%s", server.URL, path), server.Close
+}
+
+// Echo a JSON response with the request's info.
+func Echo(path string) (string, func()) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+
+	mux.HandleFunc(
+		path,
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+
+			_ = r.ParseForm()
+			fmt.Fprint(
+				w,
+				fmt.Sprintf(
+					`{"method":"%s","host":"%s","url":"%s","form":"%s"}`,
+					r.Method,
+					r.Host,
+					r.URL.String(),
+					r.Form.Encode(),
+				),
+			)
 		},
 	)
 
