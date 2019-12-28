a1ea40adddff1ce80d941533505d197936ada170
Hoanh An <hoanhan@bennington.edu>
2019-12-28 03:15:06 -0500

    add mock example

--- example_test.go
+++ example_test.go
@@ -31,3 +31,23 @@ func ExampleEcho() {
 
 	fmt.Printf("%+v\n", r) // Output: &{Method:GET Payload:k1=v1}
 }
+
+func ExampleMock() {
+	url, closer := requesttest.Mock(`{"status":"ok","payload":{"k1":"v1"}}`)
+	defer closer()
+
+	r := new(Response)
+
+	err := request.GetJSON(
+		&request.Options{
+			URL:     url,
+			Payload: map[string]string{"k1": "v1"},
+		},
+		r,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%+v\n", r) // Output: &{Status:ok Payload:map[k1:v1]}
+}
