372365b8cae055c6ab09ed1d8101301a7ea1d9b5
Hoanh An <hoanhan@bennington.edu>
2019-12-28 03:22:53 -0500

    fix examples

--- example_test.go
+++ example_test.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 
 	"github.com/hoanhan101/request"
+	"github.com/hoanhan101/requesttest"
 )
 
 // Response describes a response from requesttest.Echo().
 type Response struct {
-	Method  string `json:"method"`
-	Payload string `json:"payload"`
+	Method  string      `json:"method"`
+	Payload interface{} `json:"payload"`
 }
 
 func ExampleEcho() {
-	url, closer := Echo()
+	url, closer := requesttest.Echo()
 	defer closer()
 
 	r := new(Response)
@@ -29,19 +30,19 @@ func ExampleEcho() {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("%+v\n", r) // Output: &{Method:GET Payload:k1=v1}
+	fmt.Printf("%+v\n", r)
+	// Output: &{Method:GET Payload:k1=v1}
 }
 
 func ExampleMock() {
-	url, closer := requesttest.Mock(`{"status":"ok","payload":{"k1":"v1"}}`)
+	url, closer := requesttest.Mock(`{"method":"GET","payload":{"k1":"k2"}}`)
 	defer closer()
 
 	r := new(Response)
 
 	err := request.GetJSON(
 		&request.Options{
-			URL:     url,
-			Payload: map[string]string{"k1": "v1"},
+			URL: url,
 		},
 		r,
 	)
@@ -49,5 +50,6 @@ func ExampleMock() {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("%+v\n", r) // Output: &{Status:ok Payload:map[k1:v1]}
+	fmt.Printf("%+v\n", r)
+	// Output: &{Method:GET Payload:map[k1:k2]}
 }
